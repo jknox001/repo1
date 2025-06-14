2e2254c8e00ea9df672aa2f14e19a14c9d30f8ff
Jeff Knox <jknox001@gmail.com>
2025-06-14 10:19:31 -0400

    adding build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# shows server info
+
+echo "=== Server Info ==="
+
+echo "my name is $HOSTNAME and this is the build script"
+
+# Current date and time
+echo "Time: $(date)"
+
+# Memory info
+echo "RAM:"
+free -h | awk '/Mem:/ {print "  Total:", $2, "| Used:", $3, "| Free:", $4}'
+
+# Disk space info for root partition
+echo "Disk (/):"
+df -h / | awk 'NR==2 {print "  Total:", $2, "| Used:", $3, "| Free:", $4}'
+
